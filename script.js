f7993eb619e9c8f4777e9bf108da76f861bd4fc1
Leticia Sanches <106995301+Lsats@users.noreply.github.com>
2024-09-25 16:30:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Selecionando pelo ID do título
+const title = document.getElementById("titulo");
+
+// Selecionando o botão que exibe "Hello"
+const helloBtn = document.getElementById("botao-ola");
+
+// Selecionando o botão que altera o título
+const changeTitleBtn = document.getElementById("btn-mudar-titulo");
+
+// Evento de clique para exibir uma mensagem "Olá"
+helloBtn.addEventListener("click", function () {
+  alert("Olá!");
+});
+
+// Evento de clique para alterar o texto do título
+changeTitleBtn.addEventListener("click", function () {
+  title.textContent = "O Titulo foi mudado utilizando JS!";
+});
+
+//Selecionar os parágrafos com a classe 'mensagem2'
+
+const mensagens = document.getElementsByClassName("mensagem2");
+
+//Criar o botão que vai modificar as frases
+
+const botaoFrases = document.createElement('button');
+botaoFrases.textContent = 'Modificar frases';
+document.querySelector('.container').appendChild(botaoFrases);
+
+//Evento de clique para modificar as frases
+botaoFrases.addEventListener('click', function(){
+    for (let i = 0; i < mensagens.length; i++){
+        mensagens[i].textContent = `Frase modificada ${i} utilizando JS`
+    }
+});
